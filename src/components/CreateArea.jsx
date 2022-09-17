44f25e42ba562e66af1481fdacd942a345b736ff
Pulindu de Silva <pulindu@Pulindus-MacBook-Air.local>
2022-09-17 16:57:34 +0530

    Add a feature. Emtpty notes can't be added.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -19,36 +19,38 @@ function CreateArea(props) {
     })
   }
   function addNote(event) {
-    setNote((preValues) => {
-      console.log("preVal ", preValues)
-      return {
-        ...preValues,
-        ["id"]: preValues.id + 1
-      }
-    })
+    if (note.title !== "" && note.content !== "") {
+      setNote((preValues) => {
+        console.log("preVal ", preValues)
+        return {
+          ...preValues,
+          ["id"]: preValues.id + 1
+        }
+      })
 
-    props.setNoteList(prevNotes => {
-      return [...prevNotes, note]
-    })
+      props.setNoteList(prevNotes => {
+        return [...prevNotes, note]
+      })
+      console.log(props.noteList);
 
-
-
-    console.log(props.noteList);
-
-    setNote((preValues) => {
-      return {
-        ...preValues,
-        title: "", content: ""
-      }
-    })
-    event.preventDefault()
+      setNote((preValues) => {
+        return {
+          ...preValues,
+          title: "", content: ""
+        }
+      })
+      event.preventDefault()
+    }else{
+      alert("Please Add a Note title and a content to your Note!")
+    }
   }
+
 
   return (
     <div>
       <form>
-        <input onChange={addText} name="title" placeholder="Title" value={note.title} />
-        <textarea onChange={addText} name="content" placeholder="Take a note..." rows="3" value={note.content} />
+        <input onChange={addText} name="title" placeholder="Title" value={note.title} required />
+        <textarea onChange={addText} name="content" placeholder="Take a note..." rows="3" value={note.content} required />
 
         <button onClick={addNote}>Add</button>
       </form>
